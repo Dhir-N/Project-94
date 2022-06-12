dcba4642a3e446189b678a100eb1967761ea1009
Dhir-N <88658011+Dhir-N@users.noreply.github.com>
2022-06-12 16:38:07 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDJmmPDInlbW0Gh0QSHbbKw-VIi7LtuEi4",
+    authDomain: "project-94-a5fa8.firebaseapp.com",
+    projectId: "project-94-a5fa8",
+    storageBucket: "project-94-a5fa8.appspot.com",
+    messagingSenderId: "96491549968",
+    appId: "1:96491549968:web:b5fc6f346f4423e94e6195"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
